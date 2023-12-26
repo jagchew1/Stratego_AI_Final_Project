8762f7f12bc19f60efe882d1c35febc45623f9a8
Jonathan Chew <jagchew@gmail.com>
2023-12-26 14:34:37 -0700

    Square class

--- code/Game.cpp
+++ code/Game.cpp
@@ -4,6 +4,37 @@
 
 #include "Game.h"
 
+StrategoGame::StrategoGame(){
+    is_game_over = false;
+    player_turn = true;
+    turn_counter = 0;
+    origin = nullptr;
+    destination = nullptr;
+}
+
+void StrategoGame::print(){
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+                switch(Board[i][j].color){
+                    case 'B':
+                        cout << "B" << Board[i][j].value;
+                        break;
+                    case 'R':
+                        cout << "R" << Board[i][j].value;
+                        break;
+                    case 'W':
+                        cout << "W";
+                        break;
+                    default:
+                        cout << "_";
+                        break;
+                }
+                cout << "  ";
+            }
+            cout << endl;
+        }
+}
+
 void StrategoGame::populate_all(){
     for(auto & i : Board){
         for(auto & j : i){
--- code/Game.h
+++ code/Game.h
@@ -22,35 +22,8 @@ private:
     int turn_counter;
 
 public:
-    StrategoGame(){
-            is_game_over = false;
-            player_turn = true;
-            turn_counter = 0;
-            origin = nullptr;
-            destination = nullptr;
-    }
-    void print(){
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                switch(Board[i][j].color){
-                    case 'B':
-                        cout << "B" << Board[i][j].value;
-                        break;
-                    case 'R':
-                        cout << "R" << Board[i][j].value;
-                        break;
-                    case 'W':
-                        cout << "W";
-                        break;
-                    default:
-                        cout << "_";
-                        break;
-                }
-                cout << "  ";
-            }
-            cout << endl;
-        }
-    }
+    StrategoGame();
+    void print();
     void populate_all();
 
 };
